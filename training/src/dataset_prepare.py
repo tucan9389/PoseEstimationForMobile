3c45ca4caf02398623f1a23d71b89840c80a2492
tucan9389 <tucan9389@gmail.com>
2019-09-09 22:38:44 +0900

    no message

--- training/src/dataset_prepare.py
+++ training/src/dataset_prepare.py
@@ -54,7 +54,7 @@ class CocoPose:
 
         if file_path is not None:
             if plot_title is not None:
-                plt.title(plot_title, y=-0.01)
+                plt.set_title(plot_title, y=-0.01)
 
             plt.savefig(file_path)
 
