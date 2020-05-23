3c28552aae91fedc8344754327afcc732af789bf
Doyoung Gwak <tucan.dev@gmail.com>
2020-05-24 00:39:28 +0900

    no message

--- training/src/networks.py
+++ training/src/networks.py
@@ -4,12 +4,12 @@
 # @FileName: data_filter.py
 # @Software: PyCharm
 
-import network_mv2_cpm
-import network_mv2_hourglass
+import training.src.network_mv2_cpm
+import training.src.network_mv2_hourglass
 
 def get_network(type, input, trainable=True):
     if type == 'mv2_cpm':
-        net, loss = network_mv2_cpm.build_network(input, trainable)
+        net, loss = training.src.network_mv2_cpm.build_network(input, trainable)
     elif type == "mv2_hourglass":
-        net, loss = network_mv2_hourglass.build_network(input, trainable)        
+        net, loss = training.src.network_mv2_hourglass.build_network(input, trainable)
     return net, loss
